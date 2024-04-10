c37f7a6b8ce3d2939925c22c238132dc96a43bd4
liawyihui <ylia0072@student.monash.edu>
2024-04-11 02:03:56 +0800

    Edit home page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lymphedema</h1>
+        <div style={{ display: "flex", marginTop: "20px" }}>
+          <h3 style={{ marginRight: "20px" }}>Home</h3>
+          <h3 style={{ marginRight: "10px" }}>Assessment</h3>
+        </div>
       </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
