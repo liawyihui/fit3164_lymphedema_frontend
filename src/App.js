aa1eabc3a11bfc74445b09e1f8f8931c2ace1e5f
easonnp <epan0007@student.monash.edu>
2024-04-12 02:52:08 +0800

    update on the website protoype

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button } from 'primereact/button';
+import logo from './mylogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Lymphedema</h1>
+        <h1>MDS02 Lymphedema Predictive Model</h1>
         <div style={{ display: "flex", marginTop: "20px" }}>
-          <h3 style={{ marginRight: "20px" }}>Home</h3>
-          <h3 style={{ marginRight: "10px" }}>Assessment</h3>
+          <Button label="Home" className="p-button-outlined p-button-lg p-button-secondary" style={{ marginRight: "20px" }} />
+          <Button label="Assessment" className="p-button-outlined p-button-lg p-button-secondary" style={{ marginRight: "50px" }} />
         </div>
       </header>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <div className="ThreeLineText-container">
+        <div className="ThreeLineText1">
+              <p><strong>Your Health,</strong></p>
+              <p><strong>Our Prediction,</strong></p>
+              <p><strong>Your Future.</strong></p>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <div className="ThreeLineText2">
+        <p>Lymphedema can be managed more effectively when</p>
+        <p>detected early. We empower you with the tools to stay</p>
+        <p>one step ahead.</p>
+      </div>
     </div>
   );
 }
-
 export default App;
